05a883f36d78f0252d01c7bdfa792f3c1351c42c
hstrejolunaoasis <htrejo@oasishoteles.com>
2025-11-28 19:13:59 -0500

    chore: update build ID and environment variables for preview and production
    - Updated BuildId in index.mjs from "i3KcWYQVv_TMEuuWYS_RV" to "TMOE19MiTFZkLYua1r6ES".
    - Modified package.json and .open-next/server-functions/default/package.json to include environment flags for preview and deploy commands.
    - Updated wrangler.toml to define environment-specific variables for preview and production.

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1764369255270,
+    __BUILD_TIMESTAMP_MS__: 1764375081100,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
